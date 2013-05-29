39dc183344e2326e8819db4296d106fc19f22293
mthiffau <mthiffau@student.cs.uwaterloo.ca>
2013-05-29 16:58:06 -0400

    Send/Recv/Reply prototypes and stubs

--- u_syscall.h
+++ u_syscall.h
@@ -11,3 +11,8 @@ tid_t MyTid(void);
 tid_t MyParentTid(void);
 void  Pass(void);
 void  Exit(void) __attribute__((noreturn));
+
+int   Send(int TID, char* msg, int msglen, char* reply, int replylen);
+int   Receive(int* TID, char* msg, int msglen);
+int   Reply(int TID, char* reply, int replylen);
+
