013ec4a0e73c30205fa5af9356c1f33c82cf0ce2
Devin Papineau <jdmpapin@uwaterloo.ca>
2013-05-29 20:40:37 -0400

    Send/Receive in either order, still no Reply

--- ipc.c
+++ ipc.c
@@ -27,6 +27,8 @@
 #define RPLY_ARG_RPLY(td)    ((const char*)(td)->regs->r1)
 #define RPLY_ARG_RPLYLEN(td) ((int)(td)->regs->r2)
 
+static void rendezvous(struct kern*, struct task_desc*, struct task_desc*);
+
 void
 ipc_send_start(struct kern *kern, struct task_desc *active)
 {
@@ -40,39 +42,23 @@ ipc_send_start(struct kern *kern, struct task_desc *active)
         return;
     }
 
-    if (TASK_STATE(srv) != TASK_STATE_SEND_BLOCKED) {
+    if (TASK_STATE(srv) == TASK_STATE_SEND_BLOCKED) {
+        rendezvous(kern, active, srv);
+    } else {
         TASK_SET_STATE(srv, TASK_STATE_RECEIVE_BLOCKED);
         task_enqueue(kern, active, &srv->senders);
-        return;
     }
-
-    assert(false /* FIXME */);
 }
 
 void
 ipc_receive_start(struct kern *kern, struct task_desc *active)
 {
-    struct task_desc *sender;
-    const char *send_msg;
-    char *recv_msg;
-    int send_msglen, recv_msglen, copy_msglen;
-
-    sender = task_dequeue(kern, &active->senders);
-    assert(sender != NULL /* FIXME */);
-
-    send_msg    = SEND_ARG_MSG(sender);
-    send_msglen = SEND_ARG_MSGLEN(sender);
-    recv_msg    = RECV_ARG_MSG(active);
-    recv_msglen = RECV_ARG_MSGLEN(active);
-    copy_msglen = (recv_msglen < send_msglen) ? recv_msglen : send_msglen;
-
-    xmemcpy(recv_msg, send_msg, copy_msglen);
-    *RECV_ARG_PTID(active) = TASK_TID(kern, sender);
-    active->regs->r0 = send_msglen;
-
-    /*TASK_SET_STATE(sender, TASK_STATE_REPLY_BLOCKED); TODO */
-    task_ready(kern, sender);
-    task_ready(kern, active);
+    struct task_desc *sender = task_dequeue(kern, &active->senders);
+    if (sender != NULL) {
+        rendezvous(kern, sender, active);
+    } else {
+        TASK_SET_STATE(active, TASK_STATE_SEND_BLOCKED);
+    }
 }
 
 void
@@ -83,4 +69,30 @@ ipc_reply_start(struct kern *kern, struct task_desc *active)
     bwprintf(COM2, "  rply    = 0x%x\n", RPLY_ARG_RPLY(active));
     bwprintf(COM2, "  rplylen = %d\n",   RPLY_ARG_RPLYLEN(active));
     task_ready(kern, active);
+}
+
+static void
+rendezvous(
+    struct kern *kern,
+    struct task_desc *sender,
+    struct task_desc *receiver)
+{
+    const char *send_msg;
+    char *recv_msg;
+    int send_msglen, recv_msglen, copy_msglen;
+
+    send_msg    = SEND_ARG_MSG(sender);
+    send_msglen = SEND_ARG_MSGLEN(sender);
+    recv_msg    = RECV_ARG_MSG(receiver);
+    recv_msglen = RECV_ARG_MSGLEN(receiver);
+    copy_msglen = (recv_msglen < send_msglen) ? recv_msglen : send_msglen;
+
+    /* Prepare for returning from Receive() */
+    xmemcpy(recv_msg, send_msg, copy_msglen);
+    *RECV_ARG_PTID(receiver) = TASK_TID(kern, sender);
+    receiver->regs->r0 = send_msglen;
+
+    /*TASK_SET_STATE(sender, TASK_STATE_REPLY_BLOCKED); TODO */
+    task_ready(kern, sender); /* FIXME FIXME FIXME FIXME */
+    task_ready(kern, receiver);
 }
--- u_init.c
+++ u_init.c
@@ -13,21 +13,21 @@ static void u_test_main(void);
 void
 u_init_main(void)
 {
-    char  msg[128];
-    tid_t sending_tid;
-    int   msglen;
-
-    Create(U_INIT_PRIORITY - 1, &u_test_main);
-    bwputstr(COM2, "receiving\n");
-    msglen = Receive(&sending_tid, msg, sizeof (msg));
-    bwprintf(COM2, "received: %d: %s\n", msglen, msg);
+    tid_t child = Create(U_INIT_PRIORITY - 1, &u_test_main);
+    bwputstr(COM2, "sending\n");
+    Send(child, "The quick brown fox jumped over the lazy dog.", 46, (void*)0, 0);
+    bwputstr(COM2, "parent quitting\n");
 }
 
 static void
 u_test_main(void)
 {
-    tid_t parent = MyParentTid();
-    bwputstr(COM2, "sending\n");
-    Send(parent, "The quick brown fox jumped over the lazy dog.", 46, (void*)0, 0);
+    char  msg[128];
+    tid_t sending_tid;
+    int   msglen;
+
+    bwputstr(COM2, "receiving\n");
+    msglen = Receive(&sending_tid, msg, sizeof (msg));
+    bwprintf(COM2, "received: %d: %s\n", msglen, msg);
     bwputstr(COM2, "quitting\n");
 }
