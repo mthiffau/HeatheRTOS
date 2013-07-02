3ce1d5dcec75881f4d7ede1517e6f1e3d0fd8211
Devin Papineau <jdmpapin@uwaterloo.ca>
2013-07-02 13:50:33 -0400

    Improve organization of train server.

--- train_srv.c
+++ train_srv.c
@@ -19,14 +19,25 @@
 #include "tcmux_srv.h"
 #include "sensor_srv.h"
 
-#define TRAIN_DEFSPEED      8
-#define TRAIN_CRAWLSPEED    2
+#define TRAIN_DEFSPEED          8
+#define TRAIN_CRAWLSPEED        2
+
+/* FIXME these should be looked-up per train */
+#define TRAIN_FRONT_OFFS_MM     30
+#define TRAIN_BACK_OFFS_MM      145
 
 enum {
     TRAINMSG_SETSPEED,
     TRAINMSG_MOVETO,
     TRAINMSG_STOP,
     TRAINMSG_SENSOR,
+};
+
+enum {
+    TRAIN_DISORIENTED,
+    TRAIN_ORIENTING,
+    TRAIN_STOPPED,
+    TRAIN_ENROUTE,
 };
 
 struct trainmsg {
@@ -44,23 +55,31 @@ struct sensor_reply {
 };
 
 struct train {
+    int                       state;
     const struct track_graph *track;
     struct tcmuxctx           tcmux;
     uint8_t                   train_id;
-    const struct track_node  *landmark;
-    const struct track_node  *dest;
+    const struct track_node  *ahead, *behind;
+    int                       ahead_mm, behind_mm;
+    const struct track_node  *path[TRACK_NODES_MAX];
+    int                       path_len, path_next;
     uint8_t                   speed;
     tid_t                     sensor_tid;
     bool                      sensor_rdy;
-    const struct track_node  *path[TRACK_NODES_MAX];
 };
 
 static void trainsrv_init(struct train *tr, struct traincfg *cfg);
 static void trainsrv_setspeed(struct train *tr, uint8_t speed);
 static void trainsrv_moveto(struct train *tr, const struct track_node *dest);
 static void trainsrv_stop(struct train *tr);
+static void trainsrv_sensor_disoriented(struct train *tr);
+static void trainsrv_sensor_orienting(
+    struct train *tr, sensors_t sensors[SENSOR_MODULES]);
+static void trainsrv_sensor_enroute(
+    struct train *tr, sensors_t sensors[SENSOR_MODULES]);
 static void trainsrv_sensor(
     struct train *tr, sensors_t sensors[SENSOR_MODULES]);
+static void trainsrv_expect_sensor(struct train *tr, int sensor, int timeout);
 
 static void trainsrv_sensor_listen(void);
 static void trainsrv_empty_reply(tid_t client);
@@ -116,11 +135,12 @@ trainsrv_main(void)
 static void
 trainsrv_init(struct train *tr, struct traincfg *cfg)
 {
+    tr->state    = TRAIN_DISORIENTED;
     tr->track    = all_tracks[cfg->track_id];
     tr->train_id = cfg->train_id;
     tr->speed    = TRAIN_DEFSPEED;
-    tr->landmark = NULL;
-    tr->dest     = NULL;
+    tr->ahead    = NULL;
+    tr->behind   = NULL;
     tcmuxctx_init(&tr->tcmux);
 
     tr->sensor_rdy = false;
@@ -133,27 +153,22 @@ trainsrv_setspeed(struct train *tr, uint8_t speed)
 {
     assert(speed > 0 && speed < 15);
     tr->speed = speed;
-    if (tr->dest != NULL)
+    if (tr->state == TRAIN_ENROUTE)
         tcmux_train_speed(&tr->tcmux, tr->train_id, speed);
 }
 
 static void
 trainsrv_moveto(struct train *tr, const struct track_node *dest)
 {
-    int i, rc, path_len, end_module, end_sensor;
-    struct sensor_reply srep = {
-        .timeout = -1,
-        .sensors = { 0 }
-    };
-
-    if (tr->landmark == NULL || !tr->sensor_rdy)
+    int i;
+    if (tr->state != TRAIN_STOPPED)
         return;
 
     /* Get path to follow */
-    path_len = track_pathfind(tr->track, tr->landmark, dest, tr->path);
-    if (path_len < 0)
+    tr->path_len = track_pathfind(tr->track, tr->ahead, dest, tr->path);
+    if (tr->path_len < 0)
         return;
-    for (i = path_len - 1; i >= 0; i--) {
+    for (i = tr->path_len - 1; i >= 0; i--) {
         if (tr->path[i]->type == TRACK_NODE_SENSOR)
             break;
     }
@@ -161,18 +176,10 @@ trainsrv_moveto(struct train *tr, const struct track_node *dest)
         return;
 
     /* Watch for last sensor on path. */
-    end_sensor  = tr->path[i]->num;
-    end_module  = end_sensor / SENSORS_PER_MODULE;
-    end_sensor %= SENSORS_PER_MODULE;
-    srep.timeout = -1;
-    srep.sensors[end_module] = 1 << end_sensor;
-    rc = Reply(tr->sensor_tid, &srep, sizeof (srep));
-    assertv(rc, rc == 0);
-    tr->sensor_rdy = false;
+    trainsrv_expect_sensor(tr, tr->path[i]->num, -1);
 
     /* Switch all switches and go. */
-    tr->dest = dest;
-    for (i = 0; i < path_len - 1; i++) {
+    for (i = 0; i < tr->path_len - 1; i++) {
         bool curved;
         if (tr->path[i]->type != TRACK_NODE_BRANCH)
             continue;
@@ -180,48 +187,93 @@ trainsrv_moveto(struct train *tr, const struct track_node *dest)
         tcmux_switch_curve(&tr->tcmux, tr->path[i]->num, curved);
     }
     tcmux_train_speed(&tr->tcmux, tr->train_id, tr->speed);
+    tr->state = TRAIN_ENROUTE;
 }
 
 static void
 trainsrv_stop(struct train *tr)
 {
-    tr->dest = NULL;
     tcmux_train_speed(&tr->tcmux, tr->train_id, 0);
+    tr->state = TRAIN_STOPPED;
+}
+
+static void
+trainsrv_sensor_disoriented(struct train *tr)
+{
+    trainsrv_expect_sensor(tr, -1, -1);
+    tcmux_train_speed(&tr->tcmux, tr->train_id, TRAIN_CRAWLSPEED);
+    tr->state = TRAIN_ORIENTING;
+}
+
+static void
+trainsrv_sensor_orienting(struct train *tr, sensors_t sensors[SENSOR_MODULES])
+{
+    int i, sensor;
+    const struct track_node *sensnode;
+    const struct track_edge *ahead;
+    tcmux_train_speed(&tr->tcmux, tr->train_id, 0);
+    for (i = 0; i < SENSOR_MODULES; i++) {
+        if (sensors[i] != 0)
+            break;
+    }
+    assert(i != SENSOR_MODULES);
+    sensor       = i * SENSORS_PER_MODULE + ctz16(sensors[i]);
+    sensnode     = &tr->track->nodes[sensor];
+    ahead        = &sensnode->edge[TRACK_EDGE_AHEAD];
+    tr->ahead    = ahead->dest;
+    tr->ahead_mm = ahead->len_mm - TRAIN_FRONT_OFFS_MM;
+    /* TODO figure out behind */
+    tr->state    = TRAIN_STOPPED;
+}
+
+static void
+trainsrv_sensor_enroute(struct train *tr, sensors_t sensors[SENSOR_MODULES])
+{
+    (void)sensors;
+    trainsrv_stop(tr);
 }
 
 static void
 trainsrv_sensor(struct train *tr, sensors_t sensors[SENSOR_MODULES])
 {
-    if (tr->landmark == NULL) {
-        int i;
-        bool all_zero = true;
-        for (i = 0; i < SENSOR_MODULES; i++) {
-            if (sensors[i] != 0) {
-                all_zero = false;
-                break;
-            }
-        }
-        if (all_zero) {
-            /* Startup! Find the train. */
-            struct sensor_reply reply;
-            int rc;
-            reply.timeout = -1;
-            for (i = 0; i < SENSOR_MODULES; i++)
-                reply.sensors[i] = (sensors_t)-1;
-            rc = Reply(tr->sensor_tid, &reply, sizeof (reply));
-            assertv(rc, rc == 0);
-            tcmux_train_speed(&tr->tcmux, tr->train_id, TRAIN_CRAWLSPEED);
-        } else {
-            /* Startup part two! A sensor came in! */
-            tcmux_train_speed(&tr->tcmux, tr->train_id, 0);
-            int sensor = i * SENSORS_PER_MODULE + ctz16(sensors[i]);
-            tr->landmark = &tr->track->nodes[sensor];
-            tr->sensor_rdy = true;
-        }
-    } else {
-        trainsrv_stop(tr);
-        tr->sensor_rdy = true;
+    tr->sensor_rdy = true;
+    switch (tr->state) {
+    case TRAIN_DISORIENTED:
+        trainsrv_sensor_disoriented(tr);
+        break;
+    case TRAIN_ORIENTING:
+        trainsrv_sensor_orienting(tr, sensors);
+        break;
+    case TRAIN_ENROUTE:
+        trainsrv_sensor_enroute(tr, sensors);
+        break;
+    default:
+        panic("unexpected sensor reading in train state %d", tr->state);
     }
+}
+
+static void
+trainsrv_expect_sensor(struct train *tr, int sensor, int timeout)
+{
+    struct sensor_reply reply;
+    int i, rc;
+    assert(tr->sensor_rdy);
+    reply.timeout = timeout;
+    if (sensor == -1) {
+        for (i = 0; i < SENSOR_MODULES; i++)
+            reply.sensors[i] = (sensors_t)-1;
+    } else {
+        int module;
+        assert(sensor >= 0 && sensor < SENSOR_MODULES * SENSORS_PER_MODULE);
+        for (i = 0; i < SENSOR_MODULES; i++)
+            reply.sensors[i] = 0;
+        module  = sensor / SENSORS_PER_MODULE;
+        sensor %= SENSORS_PER_MODULE;
+        reply.sensors[module] = 1 << sensor;
+    }
+    rc = Reply(tr->sensor_tid, &reply, sizeof (reply));
+    assertv(rc, rc == 0);
+    tr->sensor_rdy = false;
 }
 
 static void
