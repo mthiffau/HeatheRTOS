df5ad123451e3275d76caf8ec8e4ead2ba56081e
Devin Papineau <jdmpapin@uwaterloo.ca>
2013-07-26 23:04:59 -0400

    Add randrange() function and improve comments in xrand.[ch]

--- xrand.c
+++ xrand.c
@@ -2,7 +2,10 @@
 #include "xrand.h"
 
 #include "xbool.h"
+#include "xassert.h"
 #include "timer.h"
+
+#define RAND_SPAN   (RAND_MAX + 1)
 
 /* Linear congruential generator:
  *     https://en.wikipedia.org/wiki/Linear_congruential_generator
@@ -28,6 +31,18 @@ rand(struct rand *r)
     const int c = 12345;
     r->seed *= a;
     r->seed += c;
-    r->seed &= 0x7fffffff;
+    r->seed &= RAND_MAX; /* RAND_MAX is of the form 2^k-1 */
     return r->seed;
+}
+
+uint32_t
+randrange(struct rand *r, uint32_t a, uint32_t b)
+{
+    /* Use float to avoid overflow. */
+    float x;
+    assert(a < b);
+    x  = (float)rand(r) / (float)RAND_SPAN; /* in [0, 1)   */
+    x *= (float)(b - a);                    /* in [0, b-a) */
+    x += (float)a;                          /* in [a, b)   */
+    return (uint32_t)x;
 }
--- xrand.h
+++ xrand.h
@@ -6,10 +6,20 @@
 
 XINT_H;
 
+#define RAND_MAX    0x7fffffffu
+
 struct rand {
     uint32_t seed;
 };
 
+/* Initialize a random number generator state with the given seed. */
 void rand_init(struct rand *r, uint32_t seed);
+
+/* Initialize a random number generator state with the debug timer as seed. */
 void rand_init_time(struct rand *r);
+
+/* Generate the next pseudo-random number in the sequence. */
 uint32_t rand(struct rand *r);
+
+/* Generate a pseudo-random number in [a, b). */
+uint32_t randrange(struct rand *r, uint32_t a, uint32_t b);
