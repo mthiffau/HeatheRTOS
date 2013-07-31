1ea52ccb83d607bad1c06d296a1675c37aff8ed9
Devin Papineau <jdmpapin@uwaterloo.ca>
2013-07-30 20:56:16 -0400

    Delay a second before calling Panic() from userspace.

--- xassert.c
+++ xassert.c
@@ -12,6 +12,7 @@
 
 #include "u_tid.h"
 #include "u_syscall.h"
+#include "clock_srv.h"
 
 static void uart_ctrl_delay(void);
 
@@ -21,6 +22,7 @@ panic(const char *fmt, ...)
     va_list args;
     if (cur_cpumode() == MODE_USR) {
         /* User-mode panic. Use Panic syscall */
+        struct clkctx clock;
         struct ringbuf buf;
         char mem[4096]; /* failing an assertion in rbuf_putc is BAD */
         rbuf_init(&buf, mem, sizeof (mem));
@@ -28,6 +30,9 @@ panic(const char *fmt, ...)
         rbuf_vprintf(&buf, fmt, args);
         va_end(args);
         rbuf_putc(&buf, '\0');
+        clkctx_init(&clock);
+        if (clock.clksrv_tid != MyTid())
+            Delay(&clock, 100); /* allow log messages to go out */
         Panic(mem);
     } else {
         /* Kernel mode panic. Use bwio and hang. */
