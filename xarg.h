79003014a5364164cfb631d9f185313e7aed7e76
Matt Thiffault <matt.thiffault@gmail.com>
2014-05-16 17:52:43 -0400

    Added copyright headers and the copyright text to the repository. We are now under GPLv2!

--- xarg.h
+++ xarg.h
@@ -1,3 +1,23 @@
+/*******************************************************************************
+    Copyright 2014 Matthew Thiffault
+
+    This file is part of HeatheRTOS.
+
+    HeatheRTOS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    HeatheRTOS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HeatheRTOS.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************/
+
 #ifndef XARG_H
 #define XARG_H
 
