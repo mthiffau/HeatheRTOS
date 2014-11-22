82ec464c5bcc42216c8b7c1da2186c34446f8322
Matt Thiffault <matt.thiffault@gmail.com>
2014-11-22 01:50:23 -0500

    Fixed the header file ifdefs.

--- apps/bbb/u_init.h
+++ apps/bbb/u_init.h
@@ -51,13 +51,12 @@
 *
 */
 
-#ifdef U_INIT_H
-#error "double-included u_init.h"
-#endif
-
+#ifndef U_INIT_H
 #define U_INIT_H
 
 enum { U_INIT_PRIORITY = 1 };
 
 /* Entry point for init */
 void u_init_main(void);
+
+#endif
